ab71c6d63fda498c4a7c79867f4b7c88bd43d28a
ParthGanjoo <parth21342@iiitd.ac.in>
2025-04-23 12:21:10 +0530

    About fix

--- app/Components/BigAbout.jsx
+++ app/Components/BigAbout.jsx
@@ -28,9 +28,9 @@ export default function BigAbout() {
               Hi, I’m Parth Ganjoo, a 21 year old Computer Science and Design student at IIIT Delhi. I’m a passionate Product Designer, a hands on Frontend and XR Developer, and a lifelong Wildlife Photographer.          </p>
             <p className="mb-4 text-base md:text-lg leading-relaxed">
               Design has always been at the heart of everything I do. From obsessively sketching cars and houses as a kid to building odd inventions from scrap during summer holidays, I was constantly exploring how things looked, felt, and worked. I chose this degree that I'm pursuing because I knew I didn’t want design to just remain a passion, I wanted to take it to a professional level and make real impact through it, helping me build a designer’s eye and a developer’s mindset.          </p>
-
+              <p className="mb-4 text-base md:text-lg leading-relaxed">
             Alongside design, I’ve been birdwatching since 2011 and photographing wildlife since 2018. I am always enthusiastic about going out in the wild and taking photos. Being in the wild shaped my observational skills and attention to detail, which are qualities that naturally flow into my design process.
-
+          </p>
             <p className="mb-4 text-base md:text-lg leading-relaxed">
               When I’m not designing or building things, you’ll probably find me chasing birds with my camera, singing and playing my guitar, or listening to old rock! (P.S. The Moon on the homepage? Shot by me on my terrace!)
             </p>
