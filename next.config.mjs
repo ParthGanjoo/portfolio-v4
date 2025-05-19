d466135b7a16726c2c1e6c97beac0a590f34c21e
ParthGanjoo <parth21342@iiitd.ac.in>
2025-05-19 12:35:38 +0530

    SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
 
-const sitemap = require('next-sitemap');
-module.exports = sitemap;
+};
 
 export default nextConfig;
-
-
-
